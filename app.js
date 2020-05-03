52f61039e8937b1fb2675cc2af834c5e3859f820
jsw13 <joseph.sze.wong@gmail.com>
2020-05-02 21:06:20 -0700

    dev commit test

---
+++ app.js
@@ -0,0 +1,16 @@
+// app.js
+
+function addNum(x,y) {
+    return(x + y)
+}
+
+function subtractNum(x,y) {
+    return(x - y)
+}
+
+function multiplyNum(x,y) {
+    return(x * y)
+}
+
+console.log(addNum(1,2))
+console.log(subtract(10,9))
